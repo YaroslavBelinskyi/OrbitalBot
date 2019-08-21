3c12102b753e68c583d433c98daec45770c85475
Yaroslav Belinskyi <yaroslav.me@gmail.com>
2019-08-21 14:22:34 +0300

    added jokes from bash

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ process.env.NTBA_FIX_319 = 1;
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const phrases = require('./phrases');
-const getArticles = require('./lib/axios-requests/forum');
+const { getArticlesOvers, getArticlesFootball, getRandomBashJoke, } = require('./lib/axios-requests/forums');
 
 const token = process.env.TOKEN;
 const bot = new TelegramBot(token, { polling: true });
@@ -10,7 +10,7 @@ const bot = new TelegramBot(token, { polling: true });
 // phrases = {'вов'}, {'ла2'}, {'линейка'}
 
 const mealTime = ['11:45', '11:50'];
-const mealOrder = ['16:30', '17:01'];
+const mealOrder = ['16:30', '16:55'];
 
 function randomAnswer(ar) {
     const number = Math.floor((Math.random() * ar.length));
@@ -31,22 +31,60 @@ bot.on('message', (msg) => {
     }
 });
 
-bot.onText(/\/news (.+)/, async (msg, match) => {
-    const articles = await getArticles();
+bot.onText(/\/news over (.+)/, async (msg, match) => {
+    const articles = await getArticlesOvers();
     const values = Object.values(articles);
     let answer = 'Вот:\r\n';
     if (match[1].toLowerCase() === 'all') {
         for (let i = 0; i < 10; i++) {
-            answer += `[${values[i].innerHTML}]`;
+            answer += `${i + 1}. [${values[i].textContent}]`;
             answer += `(https://www.overclockers.ua${values[i].href})\r\n`;
+        }
+        // const replyOptions = {
+        //     reply_markup: {
+        //         resize_keyboard: true,
+        //         one_time_keyboard: true,
+        //         keyboard: [
+        //             ['yes'],
+        //             ['no'],
+        //         ],
+        //     },
+        // };
+        bot.sendMessage(msg.chat.id, answer, { parse_mode: 'MARKDOWN', disable_web_page_preview: true });
+        // bot.sendMessage(msg.chat.id, 'Какую выберешь?', replyOptions);
+    } else {
+        const articleNumber = parseInt(match[1], 10);
+        answer += `[${values[articleNumber - 1].textContent}]`;
+        answer += `(https://www.overclockers.ua${values[articleNumber - 1].href})\r\n`;
+        bot.sendMessage(msg.chat.id, answer, { parse_mode: 'MARKDOWN' });
+    }
+});
+
+bot.onText(/\/news fb (.+)/, async (msg, match) => {
+    const articles = await getArticlesFootball();
+    const values = Object.values(articles);
+    let answer = 'Вот:\r\n';
+    if (match[1].toLowerCase() === 'all') {
+        for (let i = 0; i < 10; i++) {
+            answer += `${i + 1}. [${values[i].innerHTML.trim()}]`;
+            answer += `(${values[i].href})\r\n`;
         }
         bot.sendMessage(msg.chat.id, answer, { parse_mode: 'MARKDOWN', disable_web_page_preview: true });
     } else {
         const articleNumber = parseInt(match[1], 10);
-        answer += `[${values[articleNumber - 1].innerHTML}]`;
-        answer += `(https://www.overclockers.ua${values[articleNumber - 1].href})\r\n`;
+        answer += `[${values[articleNumber - 1].innerHTML.trim()}]`;
+        answer += `(${values[articleNumber - 1].href})\r\n`;
         bot.sendMessage(msg.chat.id, answer, { parse_mode: 'MARKDOWN' });
     }
+});
+
+bot.onText(/бот пошути/, async (msg) => {
+    const joke = await getRandomBashJoke();
+    console.log(joke);
+    // const values = Object.values(joke);
+    // console.log(values);
+    const answer = joke.innerHTML.trim().replace(/<br>/gi, '\r\n').replace(/&lt;/gi, '<').replace(/&gt;/gi, '>');
+    bot.sendMessage(msg.chat.id, answer);
 });
 
 setInterval(() => {
